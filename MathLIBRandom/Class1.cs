109330542a5e3de1c866f8613278de18c971c7af
RyanPSN <ryangregoire286@gmail.com>
2024-10-22 22:01:15 -0400

    Removed Most Concepts

--- MathLIBRandom/Class1.cs
+++ MathLIBRandom/Class1.cs
@@ -9,10 +9,9 @@ namespace RandomMathDLL
         public static List<string> opAdd = new List<string>();
         public static List<string> names = new List<string>();
         public static List<string> wordName = new List<string>();
-
-
         public static Random r = new Random();
-        public static bool isNotCorrect = false;
+
+
 
         public int Add(int a, int b)
         {
@@ -40,10 +39,10 @@ namespace RandomMathDLL
             return message.ToUpper();
         }
 
-        public void ProjectName(string name, string x1)
+        public String ProjectName(string name, string x1)
         {
-            Console.WriteLine(name.ToUpper() + " " + x1);
-
+      
+              return name +  " " + x1;
         }
 
         public string getOperator()
@@ -76,136 +75,13 @@ namespace RandomMathDLL
         }
 
 
+
+
         public static int SetNumber(int num)
         {
             return num;
         }
 
-        public int Answer()
-        {
-            return int.Parse(Console.ReadLine());
-        }
-
-
-        public void notGetAnswer(string op, int b, int c)
-        {
-
-            while (!isNotCorrect)
-            {
-                Console.WriteLine("Did You Get The Problem: ");
-                string answer = Console.ReadLine();
-
-
-                if (answer.Equals("No"))
-                {
-                    if (op.Equals("+"))
-                    {
-                        var answeredVar = Math.Abs(b + c);
-                        foreach (var a in answeredVar.ToString())
-                        {
-                            Console.WriteLine("The Correct Answer: {0} + {2} = {3}", b, op, c);
-                            Console.ReadLine();
-
-                        }
-
-                    }
-
-
-
-
-                    if (op.Equals("-"))
-                    {
-
-                        var answeredVar = Math.Abs(b - c);
-
-                        foreach (var a in answeredVar.ToString())
-                        {
-                            Console.WriteLine("The Correct Answer: {0} - {2} = {3}", b, op, c, answeredVar);
-                            Console.ReadLine();
-
-                        }
-
-
-                    }
-
-                    if (op.Equals("*"))
-                    {
-                        var answeredVar = b * c;
-
-                        foreach (var a in answeredVar.ToString())
-                        {
-
-
-                            Console.WriteLine("The Correct Answer: {0} * {2} = {3}", b, op, c, answeredVar);
-                            Console.ReadLine();
-
-                        }
-
-
-                    }
-
-
-
-                    if (op.Equals("/"))
-                    {
-                        var answeredVar = b / c;
-
-
-                        foreach (var a in answeredVar.ToString())
-                        {
-
-                            Console.WriteLine("The Correct Answer: {0} * {2} = {3}", b, op, c, answeredVar);
-                            Console.ReadLine();
-                        }
-
-                    }
-
-                }
-
-                else if (answer.Equals("Yes"))
-                {
-
-                    Console.WriteLine("What is: {0} {1} {2}: ", b, op, c);
-
-                    int num = Answer();
-
-                    if (op.Equals("+") && num == Add(b, c))
-                    {
-                        Console.WriteLine("Correct");
-
-                        Console.ReadLine();
-                    }
-
-                    if (op.Equals("-") && num == Sub(b, c))
-                    {
-                        Console.WriteLine("Correct");
-
-                        Console.ReadLine();
-                    }
-                    if (op.Equals("*") && num == Mul(b, c))
-                    {
-                        Console.WriteLine("Correct");
-
-                        Console.ReadLine();
-                    }
-
-                    if (op.Equals("/") && num == Div(b, c))
-                    {
-                        Console.WriteLine("Correct");
-
-                        Console.ReadLine();
-                    }
-
-                    Console.ReadLine();
-                }
-
-
-
-
-                isNotCorrect = true;
-            }
-
-        }
 
         public string SetName(string name)
         {
